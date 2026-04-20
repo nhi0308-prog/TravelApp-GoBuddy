6d3cbdce535a05c747b86e34fdc985e8b46c7bbb
Plan <ltrannguyenphong@gmail.com>
2026-04-20 10:20:31 +0700

    Hoan thanh goi du lieu Category tu Firebase

---
+++ app/src/main/java/com/midterm/travelapp_gobuddy/CategoryAdapter.java
@@ -0,0 +1,55 @@
+package com.midterm.travelapp_gobuddy;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private ArrayList<Category> items;
+
+    public CategoryAdapter(ArrayList<Category> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category, parent, false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Category item = items.get(position);
+        holder.titleTxt.setText(item.getName());
+
+        int drawableResourceId = holder.itemView.getContext().getResources()
+                .getIdentifier(item.getImagePath(), "drawable", holder.itemView.getContext().getPackageName());
+
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .into(holder.picImg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt;
+        ImageView picImg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            picImg = itemView.findViewById(R.id.picImg);
+        }
+    }
+}
