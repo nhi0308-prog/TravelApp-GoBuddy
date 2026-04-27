6fe2690f18bb30324fb97025e0e48bd2b8bc3e70
Plan <ltrannguyenphong@gmail.com>
2026-04-27 15:06:36 +0700

    Hoàn thành giao diện Intro background

---
+++ app/src/main/java/com/midterm/travelapp_gobuddy/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.midterm.travelapp_gobuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
