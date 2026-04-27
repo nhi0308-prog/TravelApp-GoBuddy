eb5189f59ae29629b6e515acf571cc919499a176
tuong-vi <tuongv521@gmail.com>
2026-04-27 12:55:44 +0700

    final

--- app/src/main/java/com/midterm/travelapp_gobuddy/TicketActivity.java
+++ app/src/main/java/com/midterm/travelapp_gobuddy/TicketActivity.java
@@ -1,11 +1,13 @@
 package com.midterm.travelapp_gobuddy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.midterm.travelapp_gobuddy.ItemModel;
-import com.midterm.travelapp_gobuddy.databinding.ActivityTicketBinding;;
+import com.midterm.travelapp_gobuddy.databinding.ActivityTicketBinding;
 
 public class TicketActivity extends AppCompatActivity {
     private ActivityTicketBinding binding;
@@ -22,29 +24,37 @@ public class TicketActivity extends AppCompatActivity {
     }
 
     private void setVariable() {
-        // Load ảnh chính
-        Glide.with(this)
+        Glide.with(TicketActivity.this)
+                .load(object.getPic())
+                .into(binding.imageView6);
+        Glide.with(TicketActivity.this)
                 .load(object.getPic())
                 .into(binding.imageView6);
 
-        // Nút Back
         binding.imageView9.setOnClickListener(v -> finish());
 
-        // Thiết lập thông tin text
         binding.textView9.setText(object.getTitle());
-        binding.txtGuideNameTop.setText(object.getGuideName()); // Giả định có phương thức này
-        binding.txtTime.setText(object.getTime());
         binding.txtDuration.setText(object.getDuration());
-        binding.txtGuest.setText(String.valueOf(object.getTotalGuest()));
+        binding.txtGuideNameTop.setText(object.getGuideName());
         binding.txtGuideNameFinal.setText(object.getGuideName());
+        binding.txtTime.setText(object.getTime());
+        binding.txtGuest.setText(String.valueOf(object.getTotalGuest()));
 
-        // Xử lý sự kiện nút Chat và Call (nếu cần)
         binding.btnChat.setOnClickListener(v -> {
-            // Thêm code mở chat tại đây
+            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+            sendIntent.setData(Uri.parse("sms:" + object.getTourGuidePhone()));
+            sendIntent.putExtra("sms_body", "Type your message");
+            startActivity(sendIntent);
         });
 
-        binding.btnCall.setOnClickListener(v -> {
-            // Thêm code gọi điện tại đây
+        binding.btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = object.getTourGuidePhone();
+                Intent intent = new Intent(Intent.ACTION_DIAL,
+                        Uri.fromParts("tel", phone, null));
+                startActivity(intent);
+            }
         });
 
         binding.btnDownload.setOnClickListener(v -> {
